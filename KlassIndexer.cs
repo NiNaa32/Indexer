31a46dfb5500781dda81e79d694c666dd931e6c2
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-09-15 20:33:03 +0200

    Add project files.

---
+++ KlassIndexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class KlassIndexer
+    {
+        private string[] array = new string[5];
+        public string this [int arraysindex]
+        {
+            get
+            {
+                return array[arraysindex];
+            }
+            set
+            {
+                array[arraysindex] = value;
+            }
+        }
+    }
+    // Den föregående klassen använder denna pekare när den deklarerar en array 
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Indexer;
+
+namespace Indexers
+{
+
+    class Test
+    {
+        public static void Main()
+
+        {
+            KlassIndexer obj = new KlassIndexer();
+            // KlassIndexer skapar ett objekt obj 
+            obj[0] = "Den första";
+            obj[1] = "Den andra";
+            obj[2] = "Den tredje";
+            obj[3] = "Den fjärde";
+            obj[4] = "Den femte";
+            Console.WriteLine("Ett exempel på en indexerare");
+            Console.WriteLine();
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n", obj[0], obj[1], obj[2], obj[3], obj[4]);
+            Console.ReadKey();
+        }
+    }
+}
